f3134ad0b3cbc1631fb0be70822a5c8263eb7708
komaha124 <mmistelo22@gmail.com>
2021-01-04 23:27:59 +0200

    resalt end

---
+++ js/main.js
@@ -0,0 +1,54 @@
+
+var myCar = {
+make: "Ford",
+model: "Mustang",
+year: 1969,
+speed: 120,
+}
+
+function objToString(myCar){
+   let myCarStr = "";
+    for (let key in myCar){
+        myCarStr+= `${key}: ${myCar[key]}<br>`;
+    }
+
+    return myCarStr;
+    }
+
+function buttonInfo(){
+    let INFO = document.getElementById('info');
+    cleanFild()
+    INFO.insertAdjacentHTML('beforeend', `${objToString(myCar)}`);
+}
+
+
+function cleanFild() {
+    let compareClean = document.getElementById("info");
+    while (compareClean.firstChild) {
+        compareClean.removeChild(compareClean.firstChild);
+    }
+}
+//-------------------------------------//
+function distance() {
+    let TimeDistance = Number(document.getElementById("distanc").value);
+    let Speed = Number(myCar.speed);
+    let HoursSped = Math.floor(TimeDistance/Speed);
+    let Hour = Math.floor((TimeDistance % Speed)/2);
+    let relax = 0;
+    for(let i=1; i<= HoursSped; i++){
+        if(i % 4==0){
+            relax +=1;
+        }
+    }
+    HoursSped = HoursSped + relax;
+    if(TimeDistance % 480==0){
+        HoursSped -=1;
+    }
+
+    console.log(relax)
+
+
+
+    document.getElementById("info2").innerHTML = ('beforeend', `<p> в дороге : ${HoursSped}часов ${Hour} минут</p>`);
+}
+ 
